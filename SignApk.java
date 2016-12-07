e3ba8cf00cd6c40761fe8e9b5d358ba234e155f0
ndasjowo <heddy_joe@yahoo.com>
2016-12-07 17:37:04 +0700

    Update SignApk.java

--- SignApk.java
+++ SignApk.java
@@ -17,8 +17,8 @@
  * 1. javac SignApk.java
  * 2. Create MANIFEST.MF - ( echo Main-Class: nDasJoWo.SignApk >MANIFEST.MF )
  * 3. Create folder nDasJoWo
- * 4. Move all class(*.class) to folder nDasJoWo
- * 5. jar cvfm SignApk.jar MANIFEST.MF nDasJoWo\*.class
+ * 4. Move all class(*.class) to folder nDasJoWo\signapk
+ * 5. jar cvfm SignApk.jar MANIFEST.MF nDasJoWo\signapk\*.class
  * 6. java -jar SignApk.jar
  * Source original https://github.com/android/platform_build/blob/master/tools/signapk/SignApk.java
  * Referece: https://github.com/appium/sign/
